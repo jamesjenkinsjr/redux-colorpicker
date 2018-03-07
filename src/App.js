8dd6b23abd55047fad2cc62f6ec05097d83ea415
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-06 19:32:01 -0500

    Get props and state in sync, correct issues with store, export/import reducer into store (oops definitely need that)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  
   render() {
     return (
       <div>
-       <h1>SWAG</h1>
+       <h1>I DO see color</h1>
+       <div className="color-grid" style={{backgroundColor: this.props.hex}}>Box is me</div>
+       <input type="color" onChange={(e) => this.props.changeHex(e.target.value)}/>
       </div>
     );
   }
 }
 
-export default App;
+//actions
+const updateColor = (hex) => ({
+  type: COLORPICK,
+  payload: {
+    hex: hex
+  }
+});
+//reducer
+const initialState = {
+  hex: "#000000"
+};
+export const colorChange = (state = initialState, action) => {
+  switch (action.type) {
+    case COLORPICK:
+      return {
+        ...state,
+        hex: action.payload.hex
+      }
+      default:
+      return { 
+        ...state, 
+        hex: state.hex
+      }
+
+  }
+};
+
+//types
+const COLORPICK = 'COLORPICK';
+//dispatch
+const mapStateToProps = (state) => {
+  return {
+    hex: state.hex
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeHex: (hex) => dispatch(updateColor(hex))
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps) (App);
+
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { createStore } from 'redux';
+import { colorChange } from './App';
 const store = createStore (
+    colorChange,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
